80447c45e87537ff2aba12deca2709e256b259bf
Jedrzej <jedrzej.polaczek@gmail.com>
2017-12-05 00:30:37 +0100

    Day 4 almost solved.

--- Advent_of_Code/2017/Day04/Day04.cpp
+++ Advent_of_Code/2017/Day04/Day04.cpp
@@ -10,27 +10,37 @@ int main(int argc, char ** argv) {
 	puzzle.open("puzzle_input");
 
 	int counter = 0;
+	int counter_bad = 0;
 
 	std::string row;
 	std::string word;
 
 	while(getline( puzzle, row )) {
 		std::stringstream row_stream ( row );
-		std::vector<std::string> words_vector;
+		std::vector<std::string> words_vector;		
+		counter++;
 
 		//Building vector of words
-		while( row_stream >> word) 
-			words_vector.push_back(word);
+		row_stream >> word;
+		words_vector.push_back(word);
 
-		std::unordered_map<int, int> count;  // holds count of each encountered number 
-		for (int i = 0; i < words_vector.size(); i++)        
-    		count[ words_vector[i] ]++; 
-		
-		// for (auto &e:count)
-    	// 	std::cout << e.first <<" : "<<e.second<<std::endl; 
+		while( row_stream >> word) {
+			for( int i = 0; i < words_vector.size(); ++i) {
+				if(words_vector[i] == word) {
+					counter_bad++;
+					// std::cout<<"Word: "<<word<<" | ";
+					// for(int j = 0; j < words_vector.size(); j++) {
+					// 	std::cout<<" "<<words_vector[j]; 
+					// }
+					// std::cout<<std::endl;
+					break;
+				}					
+			}
+			words_vector.push_back(word);
+		}
 	}
 
-	std::cout<<"\n\n\n\t\t\t"<<counter<<"\n\n\n";
+	std::cout<<"\n\n\n\t\t\t"<<"All: "<<counter<<" | Bad: "<<counter_bad<<" | Correct: "<<counter-counter_bad<<"\n\n\n";
 
 	return 0;
 }
